1caae9c40d3cd150802d5cec7d0908c7708faff4
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-10-30 16:42:25 +0600

    Create Warrior.cs

---
+++ Warrior.cs
@@ -0,0 +1,12 @@
+  public class Warrior
+  {
+      public int lifespan;
+      public Warrior(int lifespan)
+      {
+          this.lifespan = lifespan;
+      }
+      public int getLifespan() 
+      {
+          return lifespan; 
+      }   
+  }
